bcfde6f1ddae9787aef6edd6a3d2c3f1c290409b
chenwei <chenwei767@gmail.com>
2020-10-23 23:47:00 +0800

    s

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,17 @@ use std::process::Command;
 fn main() -> Result<()> {
     let output = Command::new("ss")
         .arg("-t")
-        .arg("src")
+        .arg("dst")
         .arg(":9944")
         .output()
         .expect("Failed to execute command");
 
     let output = String::from_utf8(output.stdout.as_slice().to_vec())?;
-    println!("{:?}", output);
+    let lines: Vec<&str> = output.split("\n").collect();
+    let mut iter = lines.into_iter();
+    iter.next();
+    for line in iter {
+        println!("{}", line);
+    }
     Ok(())
 }
