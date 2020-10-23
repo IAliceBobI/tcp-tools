134339e8b43d410ba523771f960a6c9255f764dc
chenwei <chenwei767@gmail.com>
2020-10-23 23:31:49 +0800

    s

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use std::process::Command;
+
+fn main() -> Result<()> {
+    let output = Command::new("ss")
+        .arg("-t")
+        .arg("src")
+        .arg(":9944")
+        .output()
+        .expect("Failed to execute command");
+
+    let output = String::from_utf8(output.stdout.as_slice().to_vec());
+    let output = output.unwrap();
+    println!("{:?}", output);
+    Ok(())
 }
