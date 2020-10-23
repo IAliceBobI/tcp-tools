b5e923268d8356a3b4293191d16561f991ebd795
chenwei <chenwei767@gmail.com>
2020-10-23 23:41:41 +0800

    s

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,7 @@ fn main() -> Result<()> {
         .output()
         .expect("Failed to execute command");
 
-    let output = String::from_utf8(output.stdout.as_slice().to_vec());
-    let output = output.unwrap();
+    let output = String::from_utf8(output.stdout.as_slice().to_vec())?;
     println!("{:?}", output);
     Ok(())
 }
