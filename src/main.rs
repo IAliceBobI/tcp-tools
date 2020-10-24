885a8f84ed8b23ef3b825aaaad0df60ce35c4d5a
chenwei <chenwei767@gmail.com>
2020-10-24 11:14:30 +0800

    s

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,56 @@
+use std::str::FromStr;
+
 use anyhow::Result;
-use std::process::Command;
+use clap::{App, Arg};
+
+#[derive(Debug)]
+enum SSDirection {
+    Dst,
+    Src,
+}
+
+impl FromStr for SSDirection {
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "dst" => Ok(SSDirection::Dst),
+            "src" => Ok(SSDirection::Src),
+            _ => Err("no match"),
+        }
+    }
+}
 
 fn main() -> Result<()> {
-    let output = Command::new("ss")
-        .arg("-t")
-        .arg("dst")
-        .arg(":9944")
-        .output()
-        .expect("Failed to execute command");
+    let matches = App::new("A tcp tool set")
+        .version("1.0")
+        .author("xx <xx@xx.com>")
+        .about("A tcp tool set")
+        .subcommand(
+            App::new("ss") // The name we call argument with
+                .arg(
+                    Arg::from("<direction> 'src or dst'")
+                    .possible_values(&["src","dst"])
+                ),
+        )
+        .get_matches();
 
-    let output = String::from_utf8(output.stdout.as_slice().to_vec())?;
-    let lines: Vec<&str> = output.split("\n").collect();
-    let mut iter = lines.into_iter();
-    iter.next();
-    for line in iter {
-        println!("{}", line);
-    }
+    let t: SSDirection = matches.value_of_t("direction")?;
+
+    dbg!(&t);
+
+    // let output = Command::new("ss")
+    //     .arg("-t")
+    //     .arg("dst")
+    //     .arg(":9944")
+    //     .output()
+    //     .expect("Failed to execute command");
+    // let output = String::from_utf8(output.stdout.as_slice().to_vec())?;
+    // let lines: Vec<&str> = output.split("\n").collect();
+    // let mut iter = lines.into_iter();
+    // iter.next();
+    // for line in iter {
+    //     println!("{}", line);
+    // }
     Ok(())
 }
